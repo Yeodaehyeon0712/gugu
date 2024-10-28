9f4ef7a99aea35385ce3bf2386130ba6194c0c6c
Yeo <yeodaehyeon@gmail.com>
2024-10-28 17:46:44 +0900

    Background Manager가 Addressable에서 배경을가져와 Set하게 처리

--- gugu/Assets/02.Scripts/Manager/BackgroundManager.cs
+++ gugu/Assets/02.Scripts/Manager/BackgroundManager.cs
@@ -6,8 +6,6 @@ using UnityEngine.Tilemaps;
 public class BackgroundManager : TSingletonMono<BackgroundManager>
 {
     #region Fields
-    //나중에 int는 Stage 타입으로 변경 
-    Dictionary<int, RuleTile> backgroundTiles = new Dictionary<int, RuleTile>();
     BackgroundBlock[] backgroundBlocks;
     GameObject grid;
     #endregion
@@ -15,8 +13,6 @@ public class BackgroundManager : TSingletonMono<BackgroundManager>
     #region Initialize Method
     protected override void OnInitialize()
     {
-        LoadBackgroundTile();
-
         var clonedObject = Instantiate(Resources.Load<GameObject>("Background/Grid"), transform);
         grid = clonedObject;
         backgroundBlocks = grid.transform.GetComponentsInChildren<BackgroundBlock>();
@@ -26,29 +22,18 @@ public class BackgroundManager : TSingletonMono<BackgroundManager>
 
         IsLoad = true;
     }
-    void LoadBackgroundTile()
-    {
-        //스테이지 테이블의 인덱스와 배경 룰 타일을 딕셔너리로 로드
-        for(int i=1;i<=3;i++)
-        {
-            backgroundTiles.Add(i,Resources.Load<RuleTile>($"Background/BackgroundTile{i}"));
-        }
-    }
     #endregion
 
     #region Background Method
-    public void ShowBackgroundByStage(int stageIndex)
+    public void ShowBackgroundByStage(string stagePath)
     {
-        if (backgroundTiles.TryGetValue(stageIndex, out RuleTile value) == false)
-        {
-            Debug.LogWarning($"Wrong BackgroundIndex {stageIndex}");
-            return;
-        }
+        var backgroundTile = AddressableSystem.GetBackground(stagePath);
+
         grid.SetActive(true);
         foreach (var block in backgroundBlocks)
         {
-            block.SetBackground(value);
-            value.m_TilingRules[0].m_PerlinScale = Random.Range(1, 10) * 0.1f;
+            block.SetBackground(backgroundTile);
+            backgroundTile.m_TilingRules[0].m_PerlinScale = Random.Range(1, 10) * 0.1f;
         }
     }
     public void HideBackground()
