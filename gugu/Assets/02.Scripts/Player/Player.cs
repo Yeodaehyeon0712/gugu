189ec290d4bd5d24dbeec0668ce4c6a386c528fe
Yeo <yeodaehyeon@gmail.com>
2025-03-25 10:58:36 +0900

    Player 메서드 정리

--- gugu/Assets/02.Scripts/Player/Player.cs
+++ gugu/Assets/02.Scripts/Player/Player.cs
@@ -4,13 +4,18 @@ using UnityEngine;
 
 public static class Player 
 {
-    //Data
-    public static IngameDataProperty InGameData => ingameDataProperty;
-    static IngameDataProperty ingameDataProperty;
 
     #region Fields
+    public static IngameDataProperty InGameData => ingameDataProperty;
+    static IngameDataProperty ingameDataProperty;
     public static bool IsLoad = false;
     public static Character PlayerCharacter;
+
+    //Level
+    public static float Level => currentLevel;
+    static float currentLevel;
+    static float currentExp;
+    static float nextLevelExp;
     #endregion
 
     #region Init Method
@@ -21,55 +26,62 @@ public static class Player
     }
     public static void RegisterPlayer(Character actor)
     {
+        //Player
         PlayerCharacter = actor;
-        ingameDataProperty.InitializeIngameData();
-        //일단 이거 위치 고민
-        currentLevel = 1;
-        currentExp = 0;
-        SetNextLevelExp();      
+        ingameDataProperty.InitializeData();
+        InitLevel();
+
         //Camera
         CameraManager.Instance.RegisterFollowTarget(PlayerCharacter.transform);
         //Spawn Area
         ActorManager.Instance.RegisterSpawnAreaParent(PlayerCharacter.transform);
-
+        //UI
         UIManager.Instance.FieldUI.SetHPBar(PlayerCharacter);
     }
     public static void UnRegisterPlayer()
     {
-        if (PlayerCharacter == null) return;
+        //Player
+        PlayerCharacter.Death(0f);// To Do :: if Die Already ? 
+        PlayerCharacter = null;
+        ingameDataProperty.CleanData();
+        CleanLevel();
+
         //Camera
         CameraManager.Instance.RegisterFollowTarget(null);
         //SpawnArea
         ActorManager.Instance.RegisterSpawnAreaParent(null);
-
         //UI
-        UIManager.Instance.FieldUI.FindFieldUI<FieldUI_HPBar>(eFieldUI.HPBar).Disable();//이것도 수정
-
-        //이것도 고민해바 , 이미 죽었다면 ?
-        PlayerCharacter.Death(0f);
-        PlayerCharacter = null;
+        UIManager.Instance.FieldUI.FindFieldUI<FieldUI_HPBar>(eFieldUI.HPBar).Disable();//To Do ::Hp Bar can be more than 1 
     }
     #endregion
 
-    #region LevelUp
-    static float currentExp;
-    static float nextLevelExp;
-    static float currentLevel;
-    public static float Level => currentLevel;
-
+    #region LevelUp Method
+    static void InitLevel()
+    {
+        currentLevel = 1;
+        currentExp = 0;
+        SetNextLevelExp();
+    }
+    static void CleanLevel()
+    {
+        currentExp = 0;
+        currentLevel = 0;
+        nextLevelExp = 0;
+    }
+    // To Do : 연속 레벨업 고려하기
     public static void GetExp(float exp)
     {
         currentExp += exp;
         if (currentExp >= nextLevelExp)
             LevelUp();
     }
-    public static void LevelUp()
+    static void LevelUp()
     {
         currentLevel++;
         SetNextLevelExp();
         UIManager.Instance.LevelUpPopUpUI.Enable();
     }
-    public static void SetNextLevelExp()
+    static void SetNextLevelExp()
     {
         var nextLevel = currentLevel + 1;
         nextLevelExp = 10 + 5*(nextLevel - 1);
