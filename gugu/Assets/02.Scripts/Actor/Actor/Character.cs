fa89e7a5856ace98234137d3828265fbe77941b7
Yeo <yeodaehyeon@gmail.com>
2025-04-01 10:15:58 +0900

    캐릭터 회복 메서드 생성

--- gugu/Assets/02.Scripts/Actor/Actor/Character.cs
+++ gugu/Assets/02.Scripts/Actor/Actor/Character.cs
@@ -7,12 +7,53 @@ public class Character : Actor
     #region Fields
     public SkillComponent Skill => skillComponent;
     [SerializeField] protected SkillComponent skillComponent;
+
     public Data.CharacterData characterData;
-    public bool hasDamagedThisFrame = false;
     int collisionCount = 0;
+    float recoveryTime;
     #endregion
 
-    #region Character Method
+    #region Unity API
+    protected override void Update()
+    {
+        base.Update();
+        Recovery();
+    }
+    #endregion
+
+    #region Collision Method
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (CheckTargetLayer(collision.gameObject.layer)==false) return;
+
+        CheckCollisionState(isCollisonEnter: true);
+    }
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        if (CheckTargetLayer(collision.gameObject.layer)==false) return;
+
+        var enemy = collision.gameObject.GetComponentInParent<Enemy>();
+        var damage = enemy.Status.GetStatus(eStatusType.Might);
+
+        Hit(TimeManager.DeltaTime * damage);
+    }
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (CheckTargetLayer(collision.gameObject.layer)==false) return;
+
+        CheckCollisionState(isCollisonEnter: false);
+    }
+    void CheckCollisionState(bool isCollisonEnter)
+    {
+        collisionCount += isCollisonEnter ? 1 : -1;
+
+        if (collisionCount == (isCollisonEnter ? 1 : 0))
+            Skin.SetSkinColor(isCollisonEnter ? Color.red : Color.white);
+    }
+    #endregion
+
+    #region Actor Method
     public override void Initialize(eActorType type,int objectID)
     {
         base.Initialize(type,objectID);
@@ -29,53 +70,23 @@ public class Character : Actor
         skillComponent = new SkillComponent(this);
         characterData = DataManager.CharacterTable[objectID];
     }
-    void CheckCollisionState(bool isCollisonEnter)
-    {
-        collisionCount += isCollisonEnter ? 1 : -1;
-
-        if (collisionCount == (isCollisonEnter ? 1 : 0))
-            Skin.SetSkinColor(isCollisonEnter ? Color.red : Color.white);
-    }
-    #endregion
-
-    #region Unity API
-    //private void LateUpdate()
-    //{
-    //    hasDamagedThisFrame = false;
-    //}
-    #endregion
-
-    #region Collision Method
-
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        if (CheckTargetLayer(collision.gameObject.layer)==false) return;
-
-        CheckCollisionState(isCollisonEnter: true);
-    }
-    private void OnCollisionStay2D(Collision2D collision)
-    {
-        if (CheckTargetLayer(collision.gameObject.layer)==false /*|| hasDamagedThisFrame*/) return;
-
-        var enemy = collision.gameObject.GetComponentInParent<Enemy>();
-        var damage = enemy.Status.GetStatus(eStatusType.Might);
-        //To Do : 이 부분 수정
-
-
-        Hit(TimeManager.DeltaTime * damage);
-        //hasDamagedThisFrame = true;
-    }
-    private void OnCollisionExit2D(Collision2D collision)
-    {
-        if (CheckTargetLayer(collision.gameObject.layer)==false) return;
-
-        CheckCollisionState(isCollisonEnter: false);
-    }
-    #endregion
-
     public override void Death(float time = 2.5F)
     {
         base.Death(time);
-        Timer.SetTimer(time,true,()=> StageManager.Instance.StopStage(skipResult: false));
+        Timer.SetTimer(time, true, () => StageManager.Instance.StopStage(skipResult: false));
     }
+    #endregion
+
+    #region Character Method
+    void Recovery()
+    {
+        recoveryTime += TimeManager.DeltaTime;
+        if (recoveryTime >= 1f)
+        {
+            float recoveryVal = currentHP + Status.GetStatus(eStatusType.Recovery);
+            currentHP = Mathf.Min(recoveryVal, Status.GetStatus(eStatusType.MaxHP));
+            recoveryTime = 0f;
+        }
+    }
+    #endregion
 }
